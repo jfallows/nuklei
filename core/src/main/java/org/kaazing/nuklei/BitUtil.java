064083a46091eb2ee043289eeb0270bb1d8798bd
John Fallows <john.fallows@kaazing.com>
2014-09-02 11:57:40 -0700

    Add fromHex(String) and fromHexByteArray utility methods and support offset and limit when converting to hex.

--- core/src/main/java/org/kaazing/nuklei/BitUtil.java
+++ core/src/main/java/org/kaazing/nuklei/BitUtil.java
@@ -15,14 +15,14 @@
  */
 package org.kaazing.nuklei;
 
-import sun.misc.Unsafe;
-
-import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Field;
 import java.nio.Buffer;
 import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.security.AccessController;
 import java.security.PrivilegedExceptionAction;
+
+import sun.misc.Unsafe;
 
 /**
  * Various bit utilities
@@ -73,6 +73,34 @@ public class BitUtil
 
     private static final byte[] HEX_DIGIT_TABLE = { '0', '1', '2', '3', '4', '5', '6', '7',
                                                     '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+
+    private static final byte[] FROM_HEX_DIGIT_TABLE;
+    
+    static {
+        FROM_HEX_DIGIT_TABLE = new byte[128];
+        FROM_HEX_DIGIT_TABLE['0'] = 0x00;
+        FROM_HEX_DIGIT_TABLE['1'] = 0x01;
+        FROM_HEX_DIGIT_TABLE['2'] = 0x02;
+        FROM_HEX_DIGIT_TABLE['3'] = 0x03;
+        FROM_HEX_DIGIT_TABLE['4'] = 0x04;
+        FROM_HEX_DIGIT_TABLE['5'] = 0x05;
+        FROM_HEX_DIGIT_TABLE['6'] = 0x06;
+        FROM_HEX_DIGIT_TABLE['7'] = 0x07;
+        FROM_HEX_DIGIT_TABLE['8'] = 0x08;
+        FROM_HEX_DIGIT_TABLE['9'] = 0x09;
+        FROM_HEX_DIGIT_TABLE['a'] = 0x0a;
+        FROM_HEX_DIGIT_TABLE['A'] = 0x0a;
+        FROM_HEX_DIGIT_TABLE['b'] = 0x0b;
+        FROM_HEX_DIGIT_TABLE['B'] = 0x0b;
+        FROM_HEX_DIGIT_TABLE['c'] = 0x0c;
+        FROM_HEX_DIGIT_TABLE['C'] = 0x0c;
+        FROM_HEX_DIGIT_TABLE['d'] = 0x0d;
+        FROM_HEX_DIGIT_TABLE['D'] = 0x0d;
+        FROM_HEX_DIGIT_TABLE['e'] = 0x0e;
+        FROM_HEX_DIGIT_TABLE['E'] = 0x0e;
+        FROM_HEX_DIGIT_TABLE['f'] = 0x0f;
+        FROM_HEX_DIGIT_TABLE['F'] = 0x0f;
+    }
 
     static
     {
@@ -139,6 +167,24 @@ public class BitUtil
     }
 
     /**
+     * Generate a byte array from the hex representation of the given byte array.
+     *
+     * @param buffer to convert from a hex representation (in Big Endian)
+     * @return new byte array that is decimal representation of the passed array
+     */
+    public static byte[] fromHexByteArray(final byte[] buffer) {
+        
+        final byte[] outputBuffer = new byte[buffer.length >> 1];
+
+        for (int i = 0; i < buffer.length; i += 2)
+        {
+            outputBuffer[i >> 1] = (byte) ((FROM_HEX_DIGIT_TABLE[buffer[i]] << 4) | FROM_HEX_DIGIT_TABLE[buffer[i + 1]]);
+        }
+
+        return outputBuffer;
+    }
+
+    /**
      * Generate a byte array that is a hex representation of a given byte array.
      *
      * @param buffer to convert to a hex representation
@@ -146,11 +192,24 @@ public class BitUtil
      */
     public static byte[] toHexByteArray(final byte[] buffer)
     {
-        final byte[] outputBuffer = new byte[buffer.length << 1];
+        return toHexByteArray(buffer, 0, buffer.length);
+    }
 
-        for (int i = 0; i < (buffer.length << 1); i += 2)
+    /**
+     * Generate a byte array that is a hex representation of a given byte array.
+     *
+     * @param buffer to convert to a hex representation
+     * @param index  into the buffer
+     * @param length number of bytes to convert
+     * @return new byte array that is hex representation (in Big Endian) of the passed array
+     */
+    public static byte[] toHexByteArray(final byte[] buffer, final int index, final int length)
+    {
+        final byte[] outputBuffer = new byte[length << 1];
+
+        for (int i = 0; i < (length << 1); i += 2)
         {
-            final byte b = buffer[i >> 1]; // readability
+            final byte b = buffer[index + (i >> 1)]; // readability
 
             outputBuffer[i] = HEX_DIGIT_TABLE[(b >> 4) & 0x0F];
             outputBuffer[i + 1] = HEX_DIGIT_TABLE[b & 0x0F];
@@ -159,15 +218,35 @@ public class BitUtil
     }
 
     /**
+     * Generate a byte array from a string that is the hex representation of the given byte array.
+     *
+     * @param string to convert from a hex representation (in Big Endian)
+     * @return new byte array holding the decimal representation of the passed array
+     */
+    public static byte[] fromHex(final String string)
+    {
+        return fromHexByteArray(string.getBytes(StandardCharsets.UTF_8));
+    }
+
+    /**
      * Generate a string that is the hex representation of a given byte array.
      *
      * @param buffer to convert to a hex representation
      * @return new String holding the hex representation (in Big Endian) of the passed array
-     * @throws java.io.UnsupportedEncodingException if UTF-8 not supported
      */
-    public static String toHex(final byte[] buffer) throws UnsupportedEncodingException
+    public static String toHex(final byte[] buffer) {
+        return toHex(buffer, 0, buffer.length);
+    }
+
+    /**
+     * Generate a string that is the hex representation of a given byte array.
+     *
+     * @param buffer to convert to a hex representation
+     * @return new String holding the hex representation (in Big Endian) of the passed array
+     */
+    public static String toHex(final byte[] buffer, final int index, final int length)
     {
-        return new String(toHexByteArray(buffer), "UTF-8");
+        return new String(toHexByteArray(buffer, index, length), StandardCharsets.UTF_8);
     }
 
     /**
