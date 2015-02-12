0eec0b98bacb12d505ebabbfe0c749ccbf3686c0
kris.mcqueen <kris.mcqueen@kaazing.com>
2015-02-12 15:41:33 -0800

    Add concurrency tests (multiple clients reading/writing concurrently), refactor the testware methods to take an expected set of connection ids on which a message could appear in order to support the concurrent reads/writes.

--- core/src/test/java/org/kaazing/nuklei/net/TcpSpecificationIT.java
+++ core/src/test/java/org/kaazing/nuklei/net/TcpSpecificationIT.java
@@ -26,6 +26,10 @@ import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.charset.Charset;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.After;
 import org.junit.Before;
@@ -369,9 +373,127 @@ public class TcpSpecificationIT
         }
     }
 
+    @Test
+    @Specification("concurrent.connections/tcp.client")
+    public void shouldReceiveDataFromMultipleClients() throws Exception
+    {
+        try
+        {
+            // Start a listener, expect the client to connect
+            tcpManager.launch(dedicatedNuklei);
+
+            long attachId = tcpManagerProxy.attach(PORT, new InetAddress[0], receiveBuffer);
+
+            // Expect the port to be properly bound
+            expectMessage(TcpManagerTypeId.ATTACH_COMPLETED, attachId);
+
+            // start k3po so connects happen
+            k3po.start();
+
+            processMessages(3); // expect 3 connections
+        }
+        finally
+        {
+            k3po.finish();
+        }
+    }
+
+    @Test
+    @Specification("concurrent.connections/tcp.server")
+    public void shouldSendDataFromMultipleClients() throws Exception
+    {
+        k3po.start();
+
+        // configure the manager to connect to the expected server port
+        try
+        {
+            tcpManager.launch(dedicatedNuklei);
+
+            // create 3 connections
+            long attachId1 = tcpManagerProxy.attach(
+                    0, InetAddress.getByName("0.0.0.0"), CONNECT_PORT, InetAddress.getLoopbackAddress(), receiveBuffer);
+
+            // Expect the connection to succeed as an attach/new connection
+            expectMessage(TcpManagerTypeId.ATTACH_COMPLETED, attachId1);
+            expectMessage(TcpManagerTypeId.NEW_CONNECTION, attachId1);
+
+            long attachId2 = tcpManagerProxy.attach(
+                    0, InetAddress.getByName("0.0.0.0"), CONNECT_PORT, InetAddress.getLoopbackAddress(), receiveBuffer);
+
+            // Expect the connection to succeed as an attach/new connection
+            expectMessage(TcpManagerTypeId.ATTACH_COMPLETED, attachId2);
+            expectMessage(TcpManagerTypeId.NEW_CONNECTION, attachId2);
+
+            long attachId3 = tcpManagerProxy.attach(
+                    0, InetAddress.getByName("0.0.0.0"), CONNECT_PORT, InetAddress.getLoopbackAddress(), receiveBuffer);
+
+            // Expect the connection to succeed as an attach/new connection
+            expectMessage(TcpManagerTypeId.ATTACH_COMPLETED, attachId3);
+            expectMessage(TcpManagerTypeId.NEW_CONNECTION, attachId3);
+
+            Set<Long> expectedAttachIds = new HashSet<Long>();
+            expectedAttachIds.add(attachId1);
+            expectedAttachIds.add(attachId2);
+            expectedAttachIds.add(attachId3);
+
+            // send Hello from each client
+            byte[] toSend = "Hello".getBytes(Charset.forName("UTF-8"));
+            sendAtomicBuffer.putLong(0, attachId1); // set connection ID
+            sendAtomicBuffer.putBytes(BitUtil.SIZE_OF_LONG, toSend);
+            tcpManagerProxy.send(sendAtomicBuffer, 0, BitUtil.SIZE_OF_LONG + toSend.length);
+
+            sendAtomicBuffer.putLong(0, attachId2); // set connection ID
+            sendAtomicBuffer.putBytes(BitUtil.SIZE_OF_LONG, toSend);
+            tcpManagerProxy.send(sendAtomicBuffer, 0, BitUtil.SIZE_OF_LONG + toSend.length);
+
+            sendAtomicBuffer.putLong(0, attachId3); // set connection ID
+            sendAtomicBuffer.putBytes(BitUtil.SIZE_OF_LONG, toSend);
+            tcpManagerProxy.send(sendAtomicBuffer, 0, BitUtil.SIZE_OF_LONG + toSend.length);
+
+            // On each client expect Hello back
+            expectMessage(TcpManagerTypeId.RECEIVED_DATA, expectedAttachIds, "Hello");
+            expectMessage(TcpManagerTypeId.RECEIVED_DATA, expectedAttachIds, "Hello");
+            expectMessage(TcpManagerTypeId.RECEIVED_DATA, expectedAttachIds, "Hello");
+
+            // send Goodbye from each client
+            toSend = "Goodbye".getBytes(Charset.forName("UTF-8"));
+            sendAtomicBuffer.putLong(0, attachId1); // set connection ID
+            sendAtomicBuffer.putBytes(BitUtil.SIZE_OF_LONG, toSend);
+            tcpManagerProxy.send(sendAtomicBuffer, 0, BitUtil.SIZE_OF_LONG + toSend.length);
+
+            sendAtomicBuffer.putLong(0, attachId2); // set connection ID
+            sendAtomicBuffer.putBytes(BitUtil.SIZE_OF_LONG, toSend);
+            tcpManagerProxy.send(sendAtomicBuffer, 0, BitUtil.SIZE_OF_LONG + toSend.length);
+
+            sendAtomicBuffer.putLong(0, attachId3); // set connection ID
+            sendAtomicBuffer.putBytes(BitUtil.SIZE_OF_LONG, toSend);
+            tcpManagerProxy.send(sendAtomicBuffer, 0, BitUtil.SIZE_OF_LONG + toSend.length);
+
+            // On each client expect Goodbye back
+            expectMessage(TcpManagerTypeId.RECEIVED_DATA, expectedAttachIds, "Goodbye");
+            expectMessage(TcpManagerTypeId.RECEIVED_DATA, expectedAttachIds, "Goodbye");
+            expectMessage(TcpManagerTypeId.RECEIVED_DATA, expectedAttachIds, "Goodbye");
+
+            // close each client
+            tcpManagerProxy.closeConnection(attachId1, true);
+            tcpManagerProxy.closeConnection(attachId2, true);
+            tcpManagerProxy.closeConnection(attachId3, true);
+        }
+        catch (UnknownHostException ex)
+        {
+            throw new RuntimeException("Error getting address for localhost", ex);
+        }
+        finally
+        {
+            k3po.finish();
+        }
+    }
+
     private void expectMessage(int messageType, Long expectedAttachId)
     {
-        expectMessage(messageType, expectedAttachId, null, null);
+        Set<Long> expectedAttachIds = ((expectedAttachId == null) ?
+                null : Collections.singleton(expectedAttachId));
+        expectMessage(messageType, expectedAttachIds, null, null);
     }
 
     private void expectMessage(int messageType, long[] connectionId)
@@ -379,19 +501,26 @@ public class TcpSpecificationIT
         expectMessage(messageType, null, connectionId, null);
     }
 
-    private void expectMessage(int messageType, Long expectedAttachId, String message)
+    private void expectMessage(int messageType, Long connectionId, String message)
+    {
+        expectMessage(messageType, Collections.singleton(connectionId), null, message);
+    }
+
+    private void expectMessage(int messageType, Set<Long> expectedAttachId, String message)
     {
         expectMessage(messageType, expectedAttachId, null, message);
     }
 
-    private void expectMessage(int messageType, Long expectedAttachId, long[] connectionId, String message)
+    private void expectMessage(int messageType, Set<Long> expectedAttachIds, long[] connectionId, String message)
     {
         int messages = readOneMessage((typeId, buffer, offset, length) ->
         {
             assertThat(typeId, is(messageType));
-            if (expectedAttachId != null)
+            if (expectedAttachIds != null)
             {
-                assertThat(buffer.getLong(offset), is(expectedAttachId));
+                long actualAttachId = buffer.getLong(offset);
+                assertThat("Received attachId: " + actualAttachId + " which was not in the expected list: " +
+                        expectedAttachIds, expectedAttachIds.contains(actualAttachId));
             }
             else if (connectionId != null)
             {
@@ -408,6 +537,39 @@ public class TcpSpecificationIT
             }
         });
         assertThat(messages, is(1));
+    }
+
+    private void processMessages(int connectionCount)
+    {
+        final AtomicInteger closedConnections = new AtomicInteger(0);
+        while (closedConnections.get() < connectionCount)
+        {
+            readOneMessage((typeId, buffer, offset, length) ->
+            {
+                if (typeId == TcpManagerTypeId.NEW_CONNECTION)
+                {
+                    // do we need to track the connection id?  This is request/response
+                    // so I don't think so as the new message will contain the ID
+                    // that the response needs to be written on
+                }
+                else if (typeId == TcpManagerTypeId.EOF)
+                {
+                    closedConnections.incrementAndGet();
+                }
+                else if (typeId == TcpManagerTypeId.RECEIVED_DATA)
+                {
+                    long connectionId = buffer.getLong(offset);
+
+                    byte[] receivedMessageBytes = new byte[length - BitUtil.SIZE_OF_LONG];
+                    buffer.getBytes(offset + BitUtil.SIZE_OF_LONG, receivedMessageBytes, 0, length - BitUtil.SIZE_OF_LONG);
+
+                    // as per rupert script, send "client data 1"
+                    sendAtomicBuffer.putLong(0, connectionId); // set connection ID
+                    sendAtomicBuffer.putBytes(BitUtil.SIZE_OF_LONG, receivedMessageBytes);
+                    tcpManagerProxy.send(sendAtomicBuffer, 0, BitUtil.SIZE_OF_LONG + receivedMessageBytes.length);
+                }
+            });
+        }
     }
 
     private int readOneMessage(final MpscRingBufferReader.ReadHandler handler)
