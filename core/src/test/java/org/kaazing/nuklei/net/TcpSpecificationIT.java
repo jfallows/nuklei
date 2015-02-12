36566346244bee64a902a9fedd39c7b679a42ef4
kris.mcqueen <kris.mcqueen@kaazing.com>
2015-02-11 18:30:55 -0800

    Update to latest k3po develop SNAPSHOT in order to get start/finished semantics properly integrated with the tests.
    Test robustness:  call k3po.start() after the ports are bound for tests where Nuklei is the server.  Call k3po.finished() in the finally block to ensure proper test cleanup.
    
    Added more specification tests for send/receive.

--- core/src/test/java/org/kaazing/nuklei/net/TcpSpecificationIT.java
+++ core/src/test/java/org/kaazing/nuklei/net/TcpSpecificationIT.java
@@ -29,6 +29,7 @@ import java.nio.charset.Charset;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.DisableOnDebug;
@@ -105,20 +106,26 @@ public class TcpSpecificationIT
     @Specification("establish.connection/tcp.client")
     public void establishConnectionFromClient() throws Exception
     {
-        k3po.start();
+        try
+        {
+            // Start a listener, expect the client to connect
+            tcpManager.launch(dedicatedNuklei);
 
-        // Start a listener, expect the client to connect
-        tcpManager.launch(dedicatedNuklei);
+            long attachId = tcpManagerProxy.attach(PORT, new InetAddress[0], receiveBuffer);
 
-        long attachId = tcpManagerProxy.attach(PORT, new InetAddress[0], receiveBuffer);
+            // Expect the port to be properly bound
+            expectMessage(TcpManagerTypeId.ATTACH_COMPLETED, attachId);
 
-        // Expect the port to be properly bound
-        expectMessage(TcpManagerTypeId.ATTACH_COMPLETED, attachId);
+            // start k3po so connects happen
+            k3po.start();
 
-        // Expect the client to connect
-        expectMessage(TcpManagerTypeId.NEW_CONNECTION, (Long)null);
-
-        k3po.finish();
+            // Expect the client to connect
+            expectMessage(TcpManagerTypeId.NEW_CONNECTION, (Long)null);
+        }
+        finally
+        {
+            k3po.finish();
+        }
     }
 
     @Test
@@ -143,47 +150,55 @@ public class TcpSpecificationIT
         {
             throw new RuntimeException("Error getting address for localhost", ex);
         }
-
-        k3po.finish();
+        finally
+        {
+            k3po.finish();
+        }
     }
 
     @Test
     @Specification("bidirectional.data/tcp.client")
     public void bidirectionDataFlowWithClient() throws Exception
     {
-        k3po.start();
+        try
+        {
+            // Start a listener, expect the client to connect
+            tcpManager.launch(dedicatedNuklei);
 
-        // Start a listener, expect the client to connect
-        tcpManager.launch(dedicatedNuklei);
+            long attachId = tcpManagerProxy.attach(PORT, new InetAddress[0], receiveBuffer);
 
-        long attachId = tcpManagerProxy.attach(PORT, new InetAddress[0], receiveBuffer);
+            // Expect the port to be properly bound
+            expectMessage(TcpManagerTypeId.ATTACH_COMPLETED, attachId);
 
-        // Expect the port to be properly bound
-        expectMessage(TcpManagerTypeId.ATTACH_COMPLETED, attachId);
+            // start k3po so connects happen
+            k3po.start();
 
-        // Expect the client to connect
-        long[] connectionId = new long[1];
-        expectMessage(TcpManagerTypeId.NEW_CONNECTION, connectionId);
+            // Expect the client to connect
+            long[] connectionId = new long[1];
+            expectMessage(TcpManagerTypeId.NEW_CONNECTION, connectionId);
 
-        // as per rupert script, receive "client data 1"
-        expectMessage(TcpManagerTypeId.RECEIVED_DATA, connectionId[0], "client data 1");
+            // as per rupert script, receive "client data 1"
+            expectMessage(TcpManagerTypeId.RECEIVED_DATA, connectionId[0], "client data 1");
 
-        // as per rupert script, send "server data 1"
-        byte[] toSend = "server data 1".getBytes(Charset.forName("UTF-8"));
-        sendAtomicBuffer.putLong(0, connectionId[0]); // set connection ID
-        sendAtomicBuffer.putBytes(BitUtil.SIZE_OF_LONG, toSend);
-        tcpManagerProxy.send(sendAtomicBuffer, 0, BitUtil.SIZE_OF_LONG + toSend.length);
+            // as per rupert script, send "server data 1"
+            byte[] toSend = "server data 1".getBytes(Charset.forName("UTF-8"));
+            sendAtomicBuffer.putLong(0, connectionId[0]); // set connection ID
+            sendAtomicBuffer.putBytes(BitUtil.SIZE_OF_LONG, toSend);
+            tcpManagerProxy.send(sendAtomicBuffer, 0, BitUtil.SIZE_OF_LONG + toSend.length);
 
-        // as per rupert script, receive "client data 2"
-        expectMessage(TcpManagerTypeId.RECEIVED_DATA, connectionId[0], "client data 2");
+            // as per rupert script, receive "client data 2"
+            expectMessage(TcpManagerTypeId.RECEIVED_DATA, connectionId[0], "client data 2");
 
-        // as per rupert script, send "server data 2"
-        toSend = "server data 2".getBytes(Charset.forName("UTF-8"));
-        sendAtomicBuffer.putLong(0, connectionId[0]); // set connection ID
-        sendAtomicBuffer.putBytes(BitUtil.SIZE_OF_LONG, toSend);
-        tcpManagerProxy.send(sendAtomicBuffer, 0, BitUtil.SIZE_OF_LONG + toSend.length);
-
-        k3po.finish();
+            // as per rupert script, send "server data 2"
+            toSend = "server data 2".getBytes(Charset.forName("UTF-8"));
+            sendAtomicBuffer.putLong(0, connectionId[0]); // set connection ID
+            sendAtomicBuffer.putBytes(BitUtil.SIZE_OF_LONG, toSend);
+            tcpManagerProxy.send(sendAtomicBuffer, 0, BitUtil.SIZE_OF_LONG + toSend.length);
+        }
+        finally
+        {
+            k3po.finish();
+        }
     }
 
     @Test
@@ -196,7 +211,6 @@ public class TcpSpecificationIT
         try
         {
             tcpManager.launch(dedicatedNuklei);
-
             long attachId = tcpManagerProxy.attach(
                     0, InetAddress.getByName("0.0.0.0"), CONNECT_PORT, InetAddress.getLoopbackAddress(), receiveBuffer);
 
@@ -226,8 +240,133 @@ public class TcpSpecificationIT
         {
             throw new RuntimeException("Error getting address for localhost", ex);
         }
+        finally
+        {
+            k3po.finish();
+        }
+    }
 
-        k3po.finish();
+    @Test
+    @Specification("client.close/tcp.client")
+    public void shouldReceiveConnectionThenCloseFromClient() throws Exception
+    {
+        try
+        {
+            // Start a listener, expect the client to connect
+            tcpManager.launch(dedicatedNuklei);
+
+            long attachId = tcpManagerProxy.attach(PORT, new InetAddress[0], receiveBuffer);
+
+            // Expect the port to be properly bound
+            expectMessage(TcpManagerTypeId.ATTACH_COMPLETED, attachId);
+
+            // start k3po so connects happen
+            k3po.start();
+
+            // Expect the client to connect
+            long[] connectionId = new long[1];
+            expectMessage(TcpManagerTypeId.NEW_CONNECTION, connectionId);
+
+            // Expect the client to close
+            expectMessage(TcpManagerTypeId.EOF, connectionId[0]);
+        }
+        finally
+        {
+            k3po.finish();
+        }
+    }
+
+    @Test @Ignore
+    @Specification("client.close/tcp.server")
+    public void shouldConnectThenClose() throws Exception
+    {
+        k3po.start();
+
+        // configure the manager to connect to the expected server port
+        try
+        {
+            tcpManager.launch(dedicatedNuklei);
+            long attachId = tcpManagerProxy.attach(
+                    0, InetAddress.getByName("0.0.0.0"), CONNECT_PORT, InetAddress.getLoopbackAddress(), receiveBuffer);
+
+            // Expect the connection to succeed as an attach/new connection
+            expectMessage(TcpManagerTypeId.ATTACH_COMPLETED, attachId);
+            expectMessage(TcpManagerTypeId.NEW_CONNECTION, attachId);
+
+            // as per rupert script, send close
+            tcpManagerProxy.closeConnection(attachId, true);
+        }
+        catch (UnknownHostException ex)
+        {
+            throw new RuntimeException("Error getting address for localhost", ex);
+        }
+        finally
+        {
+            k3po.finish();
+        }
+    }
+
+    @Test
+    @Specification("client.sent.data/tcp.client")
+    public void shouldReceiveDataFromClient() throws Exception
+    {
+        try
+        {
+            // Start a listener, expect the client to connect
+            tcpManager.launch(dedicatedNuklei);
+
+            long attachId = tcpManagerProxy.attach(PORT, new InetAddress[0], receiveBuffer);
+
+            // Expect the port to be properly bound
+            expectMessage(TcpManagerTypeId.ATTACH_COMPLETED, attachId);
+
+            // start k3po so connects happen
+            k3po.start();
+
+            // Expect the client to connect
+            long[] connectionId = new long[1];
+            expectMessage(TcpManagerTypeId.NEW_CONNECTION, connectionId);
+
+            // as per rupert script, receive "client data"
+            expectMessage(TcpManagerTypeId.RECEIVED_DATA, connectionId[0], "client data");
+        }
+        finally
+        {
+            k3po.finish();
+        }
+    }
+
+    @Test
+    @Specification("client.sent.data/tcp.server")
+    public void shouldSendDataToServer() throws Exception
+    {
+        k3po.start();
+
+        // configure the manager to connect to the expected server port
+        try
+        {
+            tcpManager.launch(dedicatedNuklei);
+            long attachId = tcpManagerProxy.attach(
+                    0, InetAddress.getByName("0.0.0.0"), CONNECT_PORT, InetAddress.getLoopbackAddress(), receiveBuffer);
+
+            // Expect the connection to succeed as an attach/new connection
+            expectMessage(TcpManagerTypeId.ATTACH_COMPLETED, attachId);
+            expectMessage(TcpManagerTypeId.NEW_CONNECTION, attachId);
+
+            // as per rupert script, send "client data 1"
+            byte[] toSend = "client data".getBytes(Charset.forName("UTF-8"));
+            sendAtomicBuffer.putLong(0, attachId); // set connection ID
+            sendAtomicBuffer.putBytes(BitUtil.SIZE_OF_LONG, toSend);
+            tcpManagerProxy.send(sendAtomicBuffer, 0, BitUtil.SIZE_OF_LONG + toSend.length);
+        }
+        catch (UnknownHostException ex)
+        {
+            throw new RuntimeException("Error getting address for localhost", ex);
+        }
+        finally
+        {
+            k3po.finish();
+        }
     }
 
     private void expectMessage(int messageType, Long expectedAttachId)
@@ -273,18 +412,15 @@ public class TcpSpecificationIT
 
     private int readOneMessage(final MpscRingBufferReader.ReadHandler handler)
     {
-        try
+        int messages;
+        while ((messages = receiver.read(handler, 1)) == 0)
         {
-            int messages;
-            while ((messages = receiver.read(handler, 1)) == 0)
+            Thread.yield();
+            if (Thread.currentThread().isInterrupted())
             {
-                Thread.yield();
+                throw new RuntimeException("thread is interrupted trying to read one message");
             }
-            return messages;
         }
-        catch (Exception ex)
-        {
-            throw new RuntimeException("Exception in readOneMessage", ex);
-        }
+        return messages;
     }
 }
