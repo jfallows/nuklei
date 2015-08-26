63f05ab12dcbd292a0d9d1a60b7084a284259e0e
John Fallows <john.fallows@kaazing.com>
2015-08-26 09:45:40 -0700

    Define Nukleus Java API

---
+++ common/src/test/java/org/kaazing/nuklei/common/NukleusFactoryTest.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2015, Kaazing Corporation. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kaazing.nuklei.common;
+
+import static org.junit.Assert.assertSame;
+
+import java.io.IOException;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+
+public final class NukleusFactoryTest
+{
+
+    @Test
+    public void shouldLoadAndInject() throws IOException
+    {
+        NukleusFactory factory = NukleusFactory.instantiate();
+        TestNukleus nukleus = (TestNukleus) factory.create("test", new Options());
+        assertSame(factory, nukleus.factory());
+    }
+
+    public static final class TestNukleusFactory implements NukleusFactorySpi
+    {
+        private NukleusFactory factory;
+
+        @Resource
+        public void setFacktory(NukleusFactory factory)
+        {
+            this.factory = factory;
+        }
+
+        @Override
+        public String type()
+        {
+            return "test";
+        }
+
+        @Override
+        public Nukleus create(Options options)
+        {
+            return new TestNukleus(factory);
+        }
+
+    }
+
+    static final class TestNukleus implements Nukleus
+    {
+        private final NukleusFactory factory;
+
+        TestNukleus(NukleusFactory factory)
+        {
+            this.factory = factory;
+        }
+
+        NukleusFactory factory()
+        {
+            return factory;
+        }
+
+        @Override
+        public void run()
+        {
+            // no-op
+        }
+
+        @Override
+        public void close() throws Exception
+        {
+            // no-op
+        }
+
+    }
+}
---
+++ reaktor/src/main/java/org/kaazing/nuklei/reaktor/ReaktorNukleusFactorySpi.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2015, Kaazing Corporation. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kaazing.nuklei.reaktor;
+
+import org.kaazing.nuklei.common.NukleusFactorySpi;
+import org.kaazing.nuklei.common.Options;
+
+public final class ReaktorNukleusFactorySpi implements NukleusFactorySpi
+{
+
+    @Override
+    public String type()
+    {
+        return "reaktor";
+    }
+
+    @Override
+    public ReaktorNukleus create(Options options)
+    {
+        // TODO Auto-generated method stub
+        return new ReaktorNukleus();
+    }
+
+}
