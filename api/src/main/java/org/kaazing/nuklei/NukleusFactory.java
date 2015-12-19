6ed142d5c7576809a333bfb0a003854869e82ca7
John Fallows <john.fallows@kaazing.com>
2015-12-19 10:54:25 -0800

    Remove injection of NukleusFactory into each Nukleus to reduce coupling

--- api/src/main/java/org/kaazing/nuklei/NukleusFactory.java
+++ api/src/main/java/org/kaazing/nuklei/NukleusFactory.java
@@ -15,23 +15,16 @@
  */
 package org.kaazing.nuklei;
 
-import static java.lang.reflect.Modifier.isPublic;
-import static java.lang.reflect.Modifier.isStatic;
 import static java.util.Collections.unmodifiableMap;
 import static java.util.Objects.requireNonNull;
 import static java.util.ServiceLoader.load;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.ServiceLoader;
 
-import javax.annotation.Resource;
-
 public final class NukleusFactory
 {
-
     public static NukleusFactory instantiate()
     {
         return instantiate(load(NukleusFactorySpi.class));
@@ -49,8 +42,8 @@ public final class NukleusFactory
 
     public Nukleus create(String name, Configuration config)
     {
-        requireNonNull(name);
-        requireNonNull(config);
+        requireNonNull(name, "name");
+        requireNonNull(config, "config");
 
         NukleusFactorySpi factorySpi = factorySpisByName.get(name);
         if (factorySpi == null)
@@ -66,58 +59,7 @@ public final class NukleusFactory
         Map<String, NukleusFactorySpi> factorySpisByName = new HashMap<>();
         factories.forEach((factorySpi) -> { factorySpisByName.put(factorySpi.name(), factorySpi); });
 
-        NukleusFactory factory = new NukleusFactory(unmodifiableMap(factorySpisByName));
-        factorySpisByName.values().forEach((factorySpi) -> { inject(factorySpi, NukleusFactory.class, factory); });
-
-        return factory;
-    }
-
-    private static <T> void inject(Object target, Class<T> type, T instance)
-    {
-        try
-        {
-            Class<? extends Object> targetClass = target.getClass();
-            Method[] targetMethods = targetClass.getMethods();
-            for (Method targetMethod : targetMethods)
-            {
-                if (targetMethod.getAnnotation(Resource.class) == null)
-                {
-                    continue;
-                }
-
-                if (!isPublic(targetMethod.getModifiers()) || isStatic(targetMethod.getModifiers()))
-                {
-                    continue;
-                }
-
-                Class<?>[] targetMethodParameterTypes = targetMethod.getParameterTypes();
-                if (targetMethodParameterTypes.length != 1 || targetMethodParameterTypes[0] != type)
-                {
-                    continue;
-                }
-
-                try
-                {
-                    targetMethod.invoke(target, instance);
-                }
-                catch (IllegalArgumentException e)
-                {
-                    // failed to inject
-                }
-                catch (IllegalAccessException e)
-                {
-                    // failed to inject
-                }
-                catch (InvocationTargetException e)
-                {
-                    // failed to inject
-                }
-            }
-        }
-        catch (SecurityException e)
-        {
-            // failed to reflect
-        }
+        return new NukleusFactory(unmodifiableMap(factorySpisByName));
     }
 
     private final Map<String, NukleusFactorySpi> factorySpisByName;
